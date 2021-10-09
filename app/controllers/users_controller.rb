ae135123f2ffc05709adc6a522b1a1b97cd31f45
cof.iland <shima4113@gmail.com>
2021-10-09 04:33:20 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_action :require_user_logged_in, only: [:show]
+
   
   def show
     @user = User.find(params[:id])
@@ -20,10 +21,11 @@ class UsersController < ApplicationController
       render :new
     end
   end
-end
-
-private
+  
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+end
+
